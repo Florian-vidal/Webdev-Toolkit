26240db28993c9ff11f40abdd995e9c2a7666ac8
Florian Vidal <52402114+Florian-vidal@users.noreply.github.com>
2025-09-04 00:43:02 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-// App.jsx
-// ⚡ Composant principal qui définit la structure globale (Navbar, Footer, Routes)
+// Composant principal qui définit la structure globale (Navbar, Footer, Routes)
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
+import About from "./pages/About";
 import StickyNotes from "./components/StickyNotes";
 
 
@@ -21,14 +21,15 @@ function App() {
         <main className="flex-grow px-6 py-8 max-w-7xl mx-auto">
           <Routes>
             <Route path="/" element={<Home />} /> {/* Page d'accueil */}
-            <Route path="/category/:id" element={<Category />} /> {/* Page par catégorie */}            
+            <Route path="/category/:id" element={<Category />} /> {/* Page par catégorie */}
+            <Route path="/about" element={<About /> /> {/* Page About me */}
           </Routes>
         </main>
 
         {/* Pied de page */}
         <Footer />
 
-        {/* ✅ Le pense-bête est toujours visible */}
+        {/* Le pense-bête est toujours visible */}
       <StickyNotes />
       </div>
     </Router>
