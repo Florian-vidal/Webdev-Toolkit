e5b49d38b2d65878cc024efc02d58a0dcdc85f2f
Florian Vidal <52402114+Florian-vidal@users.noreply.github.com>
2025-10-04 13:51:17 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,17 @@ function Footer() {
   return (
     <footer className="bg-gray-800 text-white text-center p-4">
       <p>
-        © {new Date().getFullYear()} Florian Vidal | Webdev Toolkit |{" "}
-        <Link to="/about" className="hover:underline text-blue-400">
-          À propos
-        </Link>
-      </p>
+  © {new Date().getFullYear()} Florian Vidal | Webdev Toolkit |{" "}
+  <a
+    href="https://portfolio-florian-vidal.netlify.app/"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="hover:underline text-blue-400"
+  >
+    Portfolio
+  </a>
+</p>
+
     </footer>
   );
 }
